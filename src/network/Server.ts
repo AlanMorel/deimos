1b876f690a6b8c654d30654ac7d2e5f468ee751c
Alan Morel <alan@alanmorel.com>
2021-01-10 17:57:57 -0500

    Add more socket event handling

--- src/network/Server.ts
+++ src/network/Server.ts
@@ -1,4 +1,6 @@
 import * as net from "net";
+import { HexColor } from "../tools/HexColor";
+import { Logger } from "../tools/Logger";
 import { LoginPacketRouter } from "./LoginPacketRouter";
 import { PacketRouter } from "./PacketRouter";
 import { Session } from "./Session";
@@ -21,17 +23,36 @@ export class Server {
 
     public async start(): Promise<boolean> {
         this.server.on("connection", socket => {
-            const session = new Session(0, socket, this.packetRouter);
-
-            console.log(`Server received a client connection: session ${session.id} @ ${session.socket.remoteAddress}`);
-
-            this.setupConnection(session);
+            this.onConnection(socket);
         });
         this.server.listen(this.port, this.host);
         return true;
     }
 
-    private setupConnection(session: Session): void {
-        session.socket.on("data", data => session.onData(data));
+    private onConnection(socket: net.Socket) {
+        const session = new Session(0, socket, this.packetRouter);
+
+        console.log(`Server received a client connection: session ${session.id} @ ${session.socket.remoteAddress}`);
+
+        this.setupSocketEvents(session);
+    }
+
+    private setupSocketEvents(session: Session): void {
+        session.socket.setNoDelay(true);
+        session.socket.on("data", data => this.onData(session, data));
+        session.socket.on('close', hadError => this.onClose(session, hadError));
+        session.socket.on('error', error => this.onError(session, error));
+    }
+
+    public onData(session: Session, data: Buffer): void {
+        session.onData(data);
+    }
+
+    public onClose(session: Session, hadError: boolean): void {
+        Logger.log(`Session ${session.id} @ ${session.socket.remoteAddress} closed`, HexColor.RED);
+    }
+
+    public onError(session: Session, error: Error): void {
+        Logger.log(error.message, HexColor.RED);
     }
 }
