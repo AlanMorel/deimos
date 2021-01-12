7bb3e59d597960f6309e1f1f2d82972c1f93c6e6
Alan Morel <alan@alanmorel.com>
2021-01-11 21:13:05 -0500

    Add job and special item attribute enums

---
+++ src/types/jobs/Job.ts
@@ -0,0 +1,15 @@
+export enum Job {
+    None = 0,
+    Knight = 100,
+    Berserker = 200,
+    Wizard = 300,
+    Priest = 400,
+    Archer = 500,
+    HeavyGunner = 600,
+    Thief = 700,
+    Assassin = 800,
+    Runeblade = 900,
+    Striker = 1000,
+    SoulBinder = 1100,
+    GameMaster = 999
+}
---
+++ src/types/jobs/JobCode.ts
@@ -0,0 +1,15 @@
+export enum JobCode {
+    None = 0,
+    Knight = 101,
+    Berserker = 201,
+    Wizard = 301,
+    Priest = 401,
+    Archer = 501,
+    HeavyGunner = 601,
+    Thief = 701,
+    Assassin = 801,
+    Runeblade = 901,
+    Striker = 1001,
+    SoulBinder = 1101,
+    GameMaster = 999
+}
