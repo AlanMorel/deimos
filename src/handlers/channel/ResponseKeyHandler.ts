7bb8edfcbea254e6255911b782d2fc6ea3231fa3
Alan Morel <alan@alanmorel.com>
2021-01-14 12:12:45 -0500

    Start ResponseKeyHandler

---
+++ src/handlers/channel/ResponseKeyHandler.ts
@@ -0,0 +1,82 @@
+import { PacketReader } from "../../crypto/protocol/PacketReader";
+import { CharacterStorage } from "../../data/storage/CharacterStorage";
+import { ChannelSession } from "../../network/sessions/ChannelSession";
+import { BuddyListPacket } from "../../packets/BuddyListPacket";
+import { DynamicChannelPacket } from "../../packets/DynamicChannelPacket";
+import { FieldEntrancePacket } from "../../packets/FieldEntrancePacket";
+import { FishingPacket } from "../../packets/FishingPacket";
+import { FurnishingInventoryPacket } from "../../packets/FurnishingInventoryPacket";
+import { KeyTablePacket } from "../../packets/KeyTablePacket";
+import { LoginRequiredPacket } from "../../packets/LoginRequiredPacket";
+import { MarketInventoryPacket } from "../../packets/MarketInventoryPacket";
+import { MoveResultPacket } from "../../packets/MoveResultPacket";
+import { PrestigePacket } from "../../packets/PrestigePacket";
+import { RequestClientTickSyncPacket } from "../../packets/RequestClientTickSyncPacket";
+import { RequestFieldEnterPacket } from "../../packets/RequestFieldEnterPacket";
+import { ServerEnterPacket } from "../../packets/ServerEnterPacket";
+import { SyncNumberPacket } from "../../packets/SyncNumberPacket";
+import { UserEnvPacket } from "../../packets/UserEnvPacket";
+import { ChannelPacketHandler } from "../ChannelPacketHandler";
+
+export class ResponseKeyHandler implements ChannelPacketHandler {
+
+    public handle(session: ChannelSession, packet: PacketReader): void {
+        const accountId = packet.readLong();
+        const tokenA = packet.readInt();
+        const tokenB = packet.readInt();
+
+        const characterId = BigInt(1);
+        const player = CharacterStorage.storage.getCharacter(characterId);
+
+        if (!player) {
+            return;
+        }
+
+        // TODO: set player session
+        // TODO: set channel id
+
+        session.send(MoveResultPacket.moveResult());
+
+        session.send(LoginRequiredPacket.loginRequired(accountId));
+
+        session.send(BuddyListPacket.startList());
+        session.send(BuddyListPacket.endList());
+
+        session.send(RequestClientTickSyncPacket.tickSync());
+
+        session.send(DynamicChannelPacket.dynamicChannel());
+
+        session.send(ServerEnterPacket.enter(player));
+
+        session.send(SyncNumberPacket.sync());
+
+        session.send(PrestigePacket.prestige(player));
+
+        // TODO: load inventory tabs
+
+        session.send(MarketInventoryPacket.count(0));
+        session.send(MarketInventoryPacket.startList());
+        session.send(MarketInventoryPacket.endList());
+
+        session.send(FurnishingInventoryPacket.startList());
+        session.send(FurnishingInventoryPacket.endList());
+
+        const titles = [10000565, 10000251, 10000291, 10000292];
+
+        session.send(UserEnvPacket.setTitles(titles));
+        session.send(UserEnvPacket.send04());
+        session.send(UserEnvPacket.send05());
+        session.send(UserEnvPacket.send08());
+        session.send(UserEnvPacket.send09());
+        session.send(UserEnvPacket.send10());
+        session.send(UserEnvPacket.send12());
+
+        session.send(FishingPacket.loadLog());
+
+        session.send(KeyTablePacket.requestDefault());
+
+        session.send(FieldEntrancePacket.enter());
+
+        session.send(RequestFieldEnterPacket.requestEnter(player));
+    }
+}
--- src/network/routers/ChannelPacketRouter.ts
+++ src/network/routers/ChannelPacketRouter.ts
@@ -1,4 +1,5 @@
 import { RecvOp } from "../../constants/RecvOp";
+import { ResponseKeyHandler } from "../../handlers/channel/ResponseKeyHandler";
 import { ResponseVersionHandler } from "../../handlers/channel/ResponseVersionHandler";
 import { PacketRouter } from "./PacketRouter";
 
@@ -6,5 +7,6 @@ export class ChannelPacketRouter extends PacketRouter {
 
     public registerHandlers(): void {
         this.handlers.set(RecvOp.RESPONSE_VERSION, new ResponseVersionHandler());
+        this.handlers.set(RecvOp.RESPONSE_KEY, new ResponseKeyHandler());
     }
 }
