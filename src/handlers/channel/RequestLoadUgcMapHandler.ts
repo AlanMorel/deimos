caa6d1d8a3369c56fda4a8b6a295ac7188bb96a5
Alan Morel <alan@alanmorel.com>
2021-01-15 19:30:07 -0500

    Add RequestLoadUgcMapHandler

---
+++ src/handlers/channel/RequestLoadUgcMapHandler.ts
@@ -0,0 +1,11 @@
+import { PacketReader } from "../../crypto/protocol/PacketReader";
+import { ChannelSession } from "../../network/sessions/ChannelSession";
+import { LoadUgcMapPacket } from "../../packets/LoadUgcMapPacket";
+import { ChannelPacketHandler } from "../ChannelPacketHandler";
+
+export class ResponseLoadUgcMapHandler implements ChannelPacketHandler {
+
+    public handle(session: ChannelSession, packet: PacketReader): void {
+        session.send(LoadUgcMapPacket.loadMap());
+    }
+}
--- src/network/routers/ChannelPacketRouter.ts
+++ src/network/routers/ChannelPacketRouter.ts
@@ -1,4 +1,5 @@
 import { RecvOp } from "../../constants/RecvOp";
+import { ResponseLoadUgcMapHandler } from "../../handlers/channel/RequestLoadUgcMapHandler";
 import { ResponseKeyHandler } from "../../handlers/channel/ResponseKeyHandler";
 import { ResponseVersionHandler } from "../../handlers/channel/ResponseVersionHandler";
 import { PacketRouter } from "./PacketRouter";
@@ -8,5 +9,6 @@ export class ChannelPacketRouter extends PacketRouter {
     public registerHandlers(): void {
         this.handlers.set(RecvOp.RESPONSE_VERSION, new ResponseVersionHandler());
         this.handlers.set(RecvOp.RESPONSE_KEY, new ResponseKeyHandler());
+        this.handlers.set(RecvOp.REQUEST_LOAD_UGC_MAP, new ResponseLoadUgcMapHandler());
     }
 }
