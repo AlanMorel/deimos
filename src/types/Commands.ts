536f09523aa4cb495c499f164f31ad8617e78c5d
Alan Morel <alan@alanmorel.com>
2021-01-18 17:18:08 -0500

    Add new teleport command

--- src/types/Commands.ts
+++ src/types/Commands.ts
@@ -1,19 +1,43 @@
 import { ChannelSession } from "../network/sessions/ChannelSession";
 import { ChatPacket } from "../packets/ChatPacket";
+import { UserSyncPacket } from "../packets/UserSyncPacket";
 import { ChatType } from "./ChatType";
+import { CoordS } from "./coords/CoordS";
+import { SyncState } from "./SyncState";
 
 export class Commands {
 
     public static process(session: ChannelSession, command: string): boolean {
-        const args = command.toLowerCase().split(" ", 2);
+        const args = command.toLowerCase().split(" ");
 
         switch (args[0]) {
             case "coord":
-                const message = session.player.coord.toString();
-                session.send(ChatPacket.send(session, message, ChatType.All));
+                this.handleCoordCommand(session, args);
+                return true;
+            case "teleport":
+                this.handleTeleportCommand(session, args);
                 return true;
         }
 
         return false;
+    }
+
+    private static handleCoordCommand(session: ChannelSession, args: string[]): void {
+        const message = session.player.coord.toString();
+        session.send(ChatPacket.send(session, message, ChatType.All));
+    }
+
+    private static handleTeleportCommand(session: ChannelSession, args: string[]): void {
+        const x = parseInt(args[1]);
+        const y = parseInt(args[2]);
+        const z = parseInt(args[3]);
+
+        const newCoord = new CoordS(x, y, z);
+
+        const syncState = new SyncState();
+        syncState.coord = newCoord;
+
+        session.player.coord = newCoord;
+        session.field?.broadcast(UserSyncPacket.syncUser(session.player, [syncState]));
     }
 }
